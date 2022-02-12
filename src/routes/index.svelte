0ca6f823911efa32ca201fa62493d14ffb3e90fb
nicgoodman <nic.goodman@hey.com>
2022-02-12 12:43:57 -0500

    added sanity and netlify config

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,14 @@
+<script>
+	import client from './api/sanityClient.js';
+
+	const query = '*[_type == "document"]';
+    client.fetch(query).then((persons) => {
+		console.log('Persons');
+		persons.forEach((person) => {
+			console.log(`${person.name}`);
+		});
+	});
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
